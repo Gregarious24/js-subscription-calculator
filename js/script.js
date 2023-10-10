3a9c33def33493e47c6d613ce27f2512e714669a
Greg M <142060854+Gregarious24@users.noreply.github.com>
2023-10-10 16:38:55 -0700

    Updated number values

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var subTypeElement = document.querySelector(".subscription-select");
 var subDurationElement = document.querySelector(".duration-select");
 var result = document.querySelector(".result");
 var subType = "basic";
-var subDuration = Number(1);
+var subDuration = 1;
 
 subTypeElement.addEventListener("change", function (e) {
     subType = e.target.value;
@@ -18,12 +18,12 @@ subDurationElement.addEventListener("change", function (e) {
 });
 
 var updateSubscriptionDiv = function () {
-    var monthlyCost = "5";
+    var monthlyCost = 5;
     if (subType === "standard") {
-        monthlyCost = "7";
+        monthlyCost = 7;
     } else if (subType === "premium") {
-        monthlyCost = "10";
+        monthlyCost = 10;
     }
-    var total = (subDuration * monthlyCost);
+    var total = subDuration * monthlyCost;
     result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
 };
